087f6e5cf9fdc7cb89b172acbecd9bcfc0e67b3f
laksavi5 <laksavi5@gmail.com>
2026-07-31 10:47:24 +0530

    Update developer name and remove unused code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import java.time.LocalDateTime;
+
 
 public class App {
 
@@ -12,23 +12,12 @@ public class App {
         System.out.println();
 
         System.out.println("Application Name : DevOps Capstone");
-        System.out.println("Developer        : Ram Kumar");
+        System.out.println("Developer        : Lakshmi");
         System.out.println("Build Tool       : Maven");
         System.out.println("CI/CD Tool       : Jenkins");
         System.out.println("Container Tool   : Docker");
         System.out.println("Cloud Platform   : AWS EC2");
         System.out.println("Monitoring       : Prometheus & Grafana");
-        System.out.println("Java Version     : " + System.getProperty("java.version"));
-        System.out.println("Operating System : " + System.getProperty("os.name"));
-        System.out.println("User             : " + System.getProperty("user.name"));
-        System.out.println("Execution Time   : " + LocalDateTime.now());
-
-        System.out.println();
-        System.out.println("========================================");
-        System.out.println(" Maven Build Successful");
-        System.out.println(" Jenkins Pipeline Executed Successfully");
-        System.out.println(" Docker Image Created Successfully");
-        System.out.println(" DevOps CI/CD Pipeline Completed");
-        System.out.println("========================================");
+        
     }
 }
